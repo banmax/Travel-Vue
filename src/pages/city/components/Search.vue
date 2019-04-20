a0ec661ec99d9a7094d929adc256bdbb197c05aa
weimingliang <we21weimingliang@qq.com>
2019-04-20 18:16:22 +0800

    完成首页及定位页的联动

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">{{item.name}}</li>
         <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
       </ul>
     </div>
@@ -51,6 +51,12 @@ export default {
         }
         this.list = result
       },100)
+    }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.$store.commit('changeCity',city)
+      this.$router.push('/')
     }
   },
   mounted () {
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <home-header :city="city"></home-header>
-    <HomeSwiper :list="swiperList"></HomeSwiper>
+    <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
     <home-recommend :list="recommendList"></home-recommend>
     <home-weekend :list="weekendList"></home-weekend>
@@ -26,7 +26,6 @@ export default {
   },
   data () {
     return{
-      city: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
@@ -42,7 +41,6 @@ export default {
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
-        this.city = data.city,
         this.swiperList = data.swiperList
         this.iconList = data.iconList
         this.recommendList = data.recommendList
