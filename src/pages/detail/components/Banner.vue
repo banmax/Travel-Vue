cf83f0addf41741bb438ec582275a69dec2bdaf9
weimingliang <we21weimingliang@qq.com>
2019-04-22 22:59:15 +0800

    增加了详情页面动画效果

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -12,12 +12,15 @@
         </div>
       </div>
     </div>
-    <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleBannerClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleBannerClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -39,7 +42,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
